d81f994601b85764cd07badc3dcd4efea11670a5
DESKTOP-3MKTUTH\Ivor <igradiski@foi.hr>
2020-11-13 20:40:46 +0100

    Dodan pregled knjiga i load more funkcija

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,26 @@
 import axios from 'axios';
-import { response } from 'express';
 
 
 
-export function getBooks(
+export function getBook(
     limit = 10,
+    order = 'asc',
     start = 0,
-    order ='asc'
-    ){
-        return null;
+    list = ''
+) {
+    const request = axios.get(`/api/getBooks?limit=${limit}&skip=${start}&order=${order}`)
+        .then(response => {
+            if (list) {
+                return [...list,...response.data]
+            }
+            else {
+                return response.data
+            }
+        }
+
+        )
+    return {
+        type: 'GET_BOOKS',
+        payload: request
     }
+}
---
+++ client/src/containers/home_container.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getBook } from '../actions'
+import BookItem from '../widgetsUI/book_item';
+class HomeContainer extends Component {
+
+
+    componentDidMount() {
+        this.props.dispatch(getBook(1, 'desc', 0))
+    }
+
+    renderItems = (books) => (
+
+     
+        books.list ?
+            books.list.map(item => (
+                <BookItem {...item} key={item._id}/>
+            ))
+            : null
+    )
+
+    loadMore = () =>{
+        let broj = this.props.books.list.length;
+        this.props.dispatch(getBook(1, 'desc', broj,this.props.books.list))
+    }
+    render() {
+        return (
+            <div>
+                {this.renderItems(this.props.books)}
+                <div className="loadmore" onClick={this.loadMore}>
+                    Load more!
+                </div>
+            </div>
+        )
+    }
+}
+
+export default connect(function mapStateToProps(state, ownProps) {
+    return {
+        books: state.books
+    }
+})(HomeContainer)
